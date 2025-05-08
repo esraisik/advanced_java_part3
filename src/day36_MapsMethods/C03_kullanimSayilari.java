df2c03ce7babf7320856e8c6ec908cb3a9d9a4fe
isikesra <isikesra01@gmail.com>
2025-05-08 23:24:04 +0300

    29

---
+++ src/day36_MapsMethods/C03_kullanimSayilari.java
@@ -0,0 +1,20 @@
+package day36_MapsMethods;
+
+import java.util.*;
+
+public class C03_kullanimSayilari {
+
+    public static void main(String[] args) {
+
+        // Verilen harfler listesinde
+// herbir harfi ve kacar defa kullanildigini bir map olarak yazdirin
+
+        List<String> harfler = new ArrayList<>(Arrays.asList("a", "b", "b", "z", "k", "z", "k", "b", "k"));
+
+// a=1, b=3, z=2, k=3
+
+        Map<String, Integer> kullanimSayilariMap = new TreeMap<>();
+
+
+    }
+}
